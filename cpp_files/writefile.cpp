cc289a33de5826e7c1004e50dd2bf993b05dbb11
ken <audrickkena@gmail.com>
2021-10-25 20:50:02 +0800

    implement writefile

--- cpp_files/writefile.cpp
+++ cpp_files/writefile.cpp
@@ -9,9 +9,10 @@ void writefile(const vector<password*> &rainbow)
 {
     ofstream outData;
     outData.open("Rainbow.txt");
-    for(int i = 0; i < rainbow.size(); i++)
+    for(int i = 0; i < rainbow.size() - 1; i++)
     {
         outData << (*rainbow.at(i)).getPassword() << "," << (*rainbow.at(i)).getFinalHash() << endl;
     }
+    outData << (*rainbow.at(rainbow.size())).getPassword() << "," << (*rainbow.at(rainbow.size())).getFinalHash();
     outData.close();
 }
