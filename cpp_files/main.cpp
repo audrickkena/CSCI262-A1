6089257c81b37f0d372be872132394a2d37a50d6
ken <audrickkena@gmail.com>
2021-10-25 20:29:21 +0800

    added rainbow vect

--- cpp_files/main.cpp
+++ cpp_files/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) //argc stores number of command line arguments 
     string passfile = argv[1]; //argv[1] is the first argument passed when running .\Rainbow {arg1,...} as argv[0] is reserved for the name of the executable
     vector<password> passwords;
     vector<password*> rainbow;
-    mainMenu(passfile, passwords);
+    mainMenu(passfile, passwords, rainbow);
     return 0;
 }
 
